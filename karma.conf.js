7285b474611f8b11f8369e3aece3382c26c4b033
Peter McCafferty <Peter@CodepakSolutions.com>
2025-11-10 21:30:13 +0000

    adding kamra file and install chrome on pipeline.

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+  
+      client: {
+        jasmine: {},
+        clearContext: false
+      },
+  
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      },
+  
+      reporters: ['progress', 'kjhtml'],
+  
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: false,
+  
+      // ✅ IMPORTANT: use headless Chrome in CI
+      browsers: process.env.CI ? ['ChromeHeadless'] : ['Chrome'],
+  
+      singleRun: true,
+      restartOnFileChange: false
+    });
+  };
+  
