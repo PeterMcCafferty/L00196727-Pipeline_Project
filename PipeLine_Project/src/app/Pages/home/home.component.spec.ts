427ee8e2912897af70ad79e4b2a1dfe3dca8a53a
Peter McCafferty <Peter@CodepakSolutions.com>
2025-11-17 19:46:49 +0000

    fixed issue with naming

--- PipeLine_Project/src/app/Pages/home/home.component.spec.ts
+++ PipeLine_Project/src/app/Pages/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 
+
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
--- PipeLine_Project/src/app/app.module.ts
+++ PipeLine_Project/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { NavComponent } from './Components/nav/nav.component';
-import { SummaryComponent } from './Pages/summary/summary.component';
-import { ComfirmationComponent } from './Pages/comfirmation/comfirmation.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { ComfirmationComponent } from './pages/comfirmation/comfirmation.component';
 
 @NgModule({
   declarations: [
